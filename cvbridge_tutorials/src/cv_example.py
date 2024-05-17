1dbb407dce83b3d8a06ff49aecc5d351067cbf7b
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-05-17 22:59:27 +0900

    Update cv_example.py

--- cvbridge_tutorials/src/cv_example.py
+++ cvbridge_tutorials/src/cv_example.py
@@ -1,52 +1,57 @@
-#!/usr/bin/env python
-from __future__ import print_function
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
-import roslib
-roslib.load_manifest('cvbridge_tutorials')
-import sys
-import rospy
-import cv2
-from std_msgs.msg import String
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-import numpy as np
-import cv2.aruco as aruco
-import yaml
+using namespace cv;
+using namespace std;
 
+class ImageConverter {
+public:
+    ImageConverter()
+        : it(nh) {
+        // Publisher를 초기화합니다.
+        image_pub = nh.advertise<sensor_msgs::Image>("cvbridge_image", 1);
+        ROS_INFO("Published to /camera/image");
 
-class image_converter:
+        // Subscriber를 초기화합니다.
+        image_sub = it.subscribe("/usb_cam/image_raw", 1, &ImageConverter::imageCallback, this);
+        ROS_INFO("Subscribed to /camera/image");
+    }
 
-  def __init__(self):
-    self.image_pub = rospy.Publisher("cvbridge_image",Image,queue_size = 1)
-    print("published to /camera/image")
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+        ROS_INFO("Callback triggered");
+        cv_bridge::CvImagePtr cv_ptr;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        } catch (cv_bridge::CvBridgeException& e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
 
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback,queue_size = 1)
-    print("subscribed to /camera/image")
- 
-  def callback(self,data):
-    print('callback')
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-    except CvBridgeError as e:
-      print(e)
+        Mat cv_image = cv_ptr->image;
+        imshow("Image window", cv_image);
+        waitKey(3);
 
-    cv2.imshow("Image window", cv_image)
-    cv2.waitKey(3)
+        try {
+            image_pub.publish(cv_ptr->toImageMsg());
+        } catch (cv_bridge::CvBridgeException& e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+        }
+    }
 
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
+private:
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it;
+    image_transport::Publisher image_pub;
+    image_transport::Subscriber image_sub;
+};
 
-def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-  main(sys.argv)
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "image_converter");
+    ImageConverter ic;
+    ros::spin();
+    destroyAllWindows();
+    return 0;
+}
